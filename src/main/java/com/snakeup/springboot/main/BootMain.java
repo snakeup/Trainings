98dde9abdd14eee0a464c9d8c62e8730333f2e90
Alberto Chacon <Alberto_Chacon@EPMXGUAW0086>
2017-10-05 14:05:57 -0500

    Initial setup configuration for spring boot training project

---
+++ src/main/java/com/snakeup/springboot/main/BootMain.java
@@ -0,0 +1,11 @@
+package com.snakeup.springboot.main;
+
+/**
+ * Created by Alberto_Chacon on 05/10/2017.
+ */
+public class BootMain {
+
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
---
+++ src/test/java/DummyTest.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Alberto_Chacon on 05/10/2017.
+ */
+public class DummyTest {
+}
